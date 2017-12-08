8912c2d5a576081d549cffd73d36cf8bbd8f59c2
Nathan V. Morrical <bitinat2@isu.edu>
2017-12-07 23:21:06 -0700

    changed settings

--- prj13/Sources/options.h
+++ prj13/Sources/options.h
@@ -8,8 +8,8 @@
 /* Camera Settings */
 #define SHOW_NORMALS false
 #define TOTAL_BOUNCES 10
-#define MAX_SAMPLES 4
-#define MIN_SAMPLES 4
+#define MAX_SAMPLES 8
+#define MIN_SAMPLES 8
 #define MAX_THRESHOLD .01
 #define MIN_THRESHOLD .000001
 #define SPHERE_BIAS .001
@@ -37,12 +37,12 @@
 #define MAX_SHADOW_SAMPLES 16
 #define MIN_SHADOW_SAMPLES 8
 
-#define DIRECT_TYPE IlluminationType::NO_ILLUMINATION
-#define INDIRECT_TYPE IlluminationType::PHOTON
+#define DIRECT_TYPE IlluminationType::PATH_TRACING
+#define INDIRECT_TYPE IlluminationType::PATH_TRACING
 #define INITIAL_PATH_TRACES 2
 
 // Path Tracing Settings
-#define INDIRECT_SAMPLES 4
+#define INDIRECT_SAMPLES 8
 
 
 // Irradiance caching
@@ -57,9 +57,9 @@
 // Photon Mapping Settings 
 /*		Photon mapping is very quick compared to path tracing, but is more difficult to get to converge.
 			Irradiance maps are helpful to smooth photon mapping out*/
-#define USE_PHOTON_MAPPING true
-#define USE_CAUSTIC_REFRACTIONS false
-#define USE_CAUSTIC_REFLECTIONS false
+#define USE_PHOTON_MAPPING false
+#define USE_CAUSTIC_REFRACTIONS true
+#define USE_CAUSTIC_REFLECTIONS true
 
 #define USE_CACHED_PHOTON_MAP true
 #define USE_CACHED_REFRACTIONS true
@@ -88,5 +88,5 @@
 
 /* Controls the brightness of the photon maps */
 #define PHOTON_SCALE (4.0 * M_PI)
-#define CAUSTIC_REFRACTION_SCALE (/*4.0 * M_PI*/1.0)
-#define CAUSTIC_REFLECTION_SCALE (/*4.0 * M_PI*/1.0)
+#define CAUSTIC_REFRACTION_SCALE (/*4.0 * M_PI*/.8)
+#define CAUSTIC_REFLECTION_SCALE (/*4.0 * M_PI*/0.8)
